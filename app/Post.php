cf9c75d12e228e799b08fdaa63d1b83e33acd2a6
Kamal Aly <kamalaly59@yahoo.com>
2018-03-17 01:29:17 +0200

    Change post table structure - Add slug and deleted_at

--- app/Post.php
+++ app/Post.php
@@ -4,11 +4,20 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+
 class Post extends Model
 {
+	use SoftDeletes;
+
+	// For mass assignment
 	protected $fillable = [
 		'title', 'featured', 'content', 'category_id'
 	];
+
+	// new timestamp
+	protected $dates = ['deleted_at'];
 
     public function category()
     {
