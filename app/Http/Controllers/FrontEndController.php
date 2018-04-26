ba2a5512997ddf3d22fde04467a895e99fe069f6
Kamal Aly <kamalaly59@yahoo.com>
2018-04-26 21:48:19 +0200

    Add Front End template - Make homepage frontend dynamic

---
+++ app/Http/Controllers/FrontEndController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Setting;
+use App\Category;
+use App\Post;
+class FrontEndController extends Controller
+{
+   public function index()
+   {
+   		return view('index')->with('title', Setting::first()->site_name)
+   							->with('categories', Category::take(5)->get())
+   							->with('last_post', Post::orderBy('created_at', 'desc')->first())
+   							->with('second_last_post', Post::orderBy('created_at', 'desc')->skip(1)->take(1)->get()->first())
+   							->with('third_last_post', Post::orderBy('created_at', 'desc')->skip(2)->take(1)->get()->first())
+   		/*TO BE CHANGED*/	->with('career', Category::find(8))
+   		/*TO BE CHANGED*/	->with('tutorials', Category::find(7))
+   							->with('settings', Setting::first());
+   }
+}
--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,10 @@ Route::get('/test', function(){
 	return App\Profile::find(1)->user;
 });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [
+	'uses' => 'FrontEndController@index',
+	'as' => 'index'
+]);
 
 Auth::routes();
 
